2f1c3ba36d4c322fea39d0f0018b9e0198cd03ee
kamadev1 <103892869+kamadev1@users.noreply.github.com>
2022-04-29 02:55:35 +0500

    Add files via upload

--- insta1.php
+++ insta1.php
@@ -1,7 +1,7 @@
 <?php
 //Kod @Clever_Coders kanali orqali tarqatildi!
 //Faqat Isntagram Rasm Va Video larini yuklaydi Reels videolarni yuklay olmaydi!!!
-$host="mazzami.com/Instagram/";
+$host="https://raw.githubusercontent.com/kamadev1/kamadev/main/";
 $get=file_get_contents($host."insta.php?url=".$_GET["url"]);
 $video = explode('"',explode('<a href="',$get)[1])[0];
 echo ($video);
